680d264c85ccc116316bc287f867378228e4a872
Akmal-M <akmalutube77@gmail.com>
2021-01-19 17:18:31 +0500

    last

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+export default {
+    products:[
+        {   _id: '1',
+            name: 'Dress',
+            category:'Cloth',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock:7
+        } ,   {
+            _id: '2',
+            name: 'Shoes',
+            category:'Shoes',
+            image: '/images/sh1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 7,
+            countInStock:0
+        } ,   {
+            _id: '3',
+            name: 'Dress',
+            category:'Cloth',
+            image: '/images/d2.jpg',
+            price: 25,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 3,
+            countInStock:5
+        } ,   {
+            _id: '4',
+            name: 'Shoes',
+            category:'Shoes',
+            image: '/images/sh2.jpg',
+            price: 30,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 9,
+            countInStock:10
+        } , {
+            _id: '5',
+            name: 'Cloth',
+            category:'Cloth',
+            image: '/images/d3.jpg',
+            price: 25,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 3,
+            countInStock:5
+        } ,
+    ]
+}
